8487b7ff344c2408ee549c51fc88e6d47c037d64
TarikaTheHorse <violette.and.john@gmail.com>
2026-06-23 14:34:43 -0700

    More design stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,16 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
+
+import alpinejs from '@astrojs/alpinejs';
+
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-  output: 'server'
+
+  output: 'server',
+  integrations: [alpinejs()]
 });
