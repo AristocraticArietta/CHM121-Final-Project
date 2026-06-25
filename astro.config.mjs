6ca561c94a58cd9e53952f2a259fecddc9d8ffaa
TarikaTheHorse <violette.and.john@gmail.com>
2026-06-25 14:43:51 -0700

    Added blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,41 @@
 // @ts-check
+
+import { defineConfig, fontProviders  } from 'astro/config';
+
 import alpinejs from '@astrojs/alpinejs';
-import { defineConfig } from 'astro/config';
+
 import tailwindcss from "@tailwindcss/vite";
+
+import mdx from '@astrojs/mdx';
+
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'http://localhost:4321',
   vite: {
     plugins: [tailwindcss()]
   },
 
   output: 'server',
-  integrations: [alpinejs()]
+  integrations: [alpinejs(), mdx(), sitemap()],
+
+  fonts: [
+    {
+      provider: fontProviders.local(),
+            name: 'Roboto',
+            cssVariable: '--font-roboto',
+            fallbacks: ['sans-serif'],
+            options: {
+                variants: [
+                    {
+                        src: ['./src/assets/Fonts/roboto/Roboto-VariableFont_wdth_wght.ttf'],
+                        weight: 400,
+                        style: 'normal',
+                        display: 'swap',
+                    },
+                ],
+            },
+    }
+  ]
 });
---
+++ src/consts.ts
@@ -0,0 +1,2 @@
+export const SITE_TITLE = 'Periodic Table';
+export const SITE_DESCRIPTION = 'CHM121IN Final Project!';
